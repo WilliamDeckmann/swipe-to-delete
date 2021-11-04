ac284ef95e00a61591c5bef0b320273e5e74fd98
WilliamDeckmann <u773560@rts-365.dk>
2021-11-04 15:27:06 +0100

    Update list and delete

--- assets/js/list.js
+++ assets/js/list.js
@@ -2,9 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Element variables
     const list = document.querySelector(".List");
+    const bin = document.querySelector(".Bin");
 
     // URL variables
     let url = "https://jsonplaceholder.typicode.com/users";
+
+    // Array variables
+    let userArray = [];
+    let deletedArray = [];
 
 
 
@@ -16,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
         for(let i = 0; i < 10; i++) {       
        
             // Response variable
-            var data = response.data[i];
+            let data = response.data[i];
 
 
             
@@ -24,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
             let item = document.createElement("li");
             list.appendChild(item);
             item.classList.add("Item")
-            //item.setAttribute("id", `Item_${i}`);
+            item.setAttribute("id", `Item_${i}`);
             
             // Create: remove
             let remove = document.createElement("div");
@@ -45,57 +50,138 @@ document.addEventListener("DOMContentLoaded", () => {
                     Username: ${data.username}
                 </p>
             `;
+
+
+
+            //  Add to local storage
+            userArray.push(data.username);
+            localStorage.setItem("User", userArray);
         };
+
+
+
+        // Add event listener
+        list.addEventListener("touchstart", (e) => {
+
+            // Target element
+            let target = e.target;
+            let parent = e.target.parentElement;
+
+            // Element variables
+            let remove = parent.querySelector(".Delete");
+            //let remove = target;
+            //let article = target;
+
+            // Touch variables
+            let touchStart = e.touches[0].clientX;
+            let touchMove;
+            let touchEnd
+
+
+            
+            // Delete
+            parent.addEventListener("click", () => {
+                if(target.classList == "Delete") {
+                    // mainElement.style.transform = "scale(0, 0)";
+                    target.style.transform = `translateX(0px)`;
+                    parent.classList.add("animate__animated", "animate__backOutLeft");
+                    setTimeout(() => {parent.classList.add("collapsed")}, 500);
+                    setTimeout(() => {parent.remove()}, 500 + 500); // Wait th time it takes for all animations to run
+
+                    //  Add to local storage
+                    deletedArray.push(response.data[parent.id.replace("Item_", "")].username);
+
+                    // Filter array - Made by: "Oliver Rindolt"
+                    function filter(a){
+                        let filtered = [];
+                        a.forEach(item =>{
+                            if(!filtered.includes(item)){
+                                filtered.push(item);
+                            };
+                        });
+                        return filtered;
+                    };
+
+                    // Add to local storage
+                    localStorage.setItem("Deleted", filter(deletedArray));
+                    console.log(localStorage.getItem("Deleted").split(","));
+
+                    DeletedList()
+                };
+            });
+
+            // Touch start
+            // target.addEventListener("touchstart", (e) => touchStart = e.touches[0].clientX);
+
+            // Touch move
+            target.addEventListener("touchmove", (e) => {
+                if(target.classList == "Article") {
+                    touchMove = Math.floor(e.touches[0].clientX);
+                    if(touchMove < touchStart && touchMove > touchStart - remove.offsetWidth) { // remove.offsetWidth // viewWidth
+                        target.style.transform = `translateX(${touchMove - touchStart}px)`;
+                    };
+                };
+            });
+
+            // Touch end
+            target.addEventListener("touchend", (e) => {
+                if(target.classList == "Article") {
+                    touchEnd = Math.floor(e.changedTouches[0].clientX);
+                    if(touchEnd < touchStart - remove.offsetWidth / 2) { // remove.offsetWidth / 2  // viewWidth / 2
+                        target.style.transform = `translateX(-${remove.offsetWidth}px)`;
+                    }else {
+                        target.style.transform = `translateX(0px)`; // ${e.target.offsetLeft}
+                    }
+                };
+            });
+        });
     });
 
 
 
-    list.addEventListener("touchstart", (e) => {
+    // Deleted list
+    function DeletedList() {
 
-        // Target element
-        let target = e.target.parentNode;
-
-        // Element variables
-        let item = target
-        let remove = target.querySelector(".Delete");
-        let article = target.querySelector(".Article");
-
-        // Touch variables
-        let touchStart = e.touches[0].clientX;
-        let touchMove;
-        let touchEnd
-
-
+        // Reset list
+        bin.innerHTML = `
+            <!-- Header -->
+            <header class="Header">
+                <a class="Reload" href="index.html">
+                    Deleted Items
+                </a>
+            </header>
+        `;
         
-        // Delete
-        remove.addEventListener("click", () => {
-            // mainElement.style.transform = "scale(0, 0)";
-            article.style.transform = `translateX(0px)`;
-            item.classList.add("animate__animated", "animate__backOutLeft");
-            setTimeout(() => {item.classList.add("collapsed")}, 500);
-            setTimeout(() => {item.remove()}, 500 + 500); // Wait th time it takes for all animations to run
-        });
-
-        // Touch start
-        // article.addEventListener("touchstart", (e) => touchStart = e.touches[0].clientX);
-
-        // Touch move
-        article.addEventListener("touchmove", (e) => {
-            touchMove = Math.floor(e.touches[0].clientX);
-            if(touchMove < touchStart && touchMove > touchStart - remove.offsetWidth) { // remove.offsetWidth // viewWidth
-                article.style.transform = `translateX(${touchMove - touchStart}px)`;
-            };
-        });
-
-        // Touch end
-        article.addEventListener("touchend", (e) => {
-            touchEnd = Math.floor(e.changedTouches[0].clientX);
-            if(touchEnd < touchStart - remove.offsetWidth / 2) { // remove.offsetWidth / 2  // viewWidth / 2
-                article.style.transform = `translateX(-${remove.offsetWidth}px)`;
-            }else {
-                article.style.transform = `translateX(0px)`; // ${e.target.offsetLeft}
-            }
-        });
+        // For each
+        localStorage.getItem("Deleted").split(",").forEach(element => {
         
-    })
+            // Create: item
+            let item = document.createElement("li");
+            bin.appendChild(item);
+            item.classList.add("Item")
+            //item.setAttribute("id", `Item_${i}`);
+            
+            // Create: remove
+            let remove = document.createElement("div");
+            item.appendChild(remove);
+            remove.classList.add("Delete")
+            remove.innerHTML = `
+                <p class="Text">
+                    Delete
+                </p>
+            `;
+    
+            // Create: article
+            let article = document.createElement("article");
+            item.appendChild(article);
+            article.classList.add("Article")
+            article.innerHTML = `
+                <p class="Text">
+                    Username: ${element}
+                </p>
+            `;
+        });
+    };
+
+    console.log(localStorage.getItem("Deleted"))
 });
